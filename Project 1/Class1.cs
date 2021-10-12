bdfb4d742ec8e7cacac381450e8626d0b427a319
Lucas Smith <lsoceanengineer@gmail.com>
2021-10-12 12:51:30 -0700

    Add project files.

---
+++ Project 1/Class1.cs
@@ -0,0 +1,55 @@
+﻿public class Counter
+{
+	private int count;
+
+	public void setCounter()
+	{
+		count = 0;
+	}
+
+	public void incCounter()
+	{
+		count += 1;
+	}
+
+		
+	public void dscCounter()
+	{
+		if (count > 0)
+		{
+			count -= 1;
+		}
+	}
+
+	public int getCount()
+	{
+		return count;
+	}
+
+	
+	public string displayCount()
+	{
+		return "Current Count Value: " + count;
+	}
+
+	
+	public string toString()
+	{
+		return "Counter [count=" + count + "]";
+	}
+
+	
+	public override bool Equals(object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		Counter other = (Counter)obj;
+		if (count != other.count)
+			return false;
+		return true;
+	}
+
+
+}
---
+++ Project 1/Program.cs
@@ -0,0 +1,48 @@
+﻿/// Week 7 Project 1
+/// File Name: week7project1.cs
+/// Author: Lucas Smith
+/// Date:  October 11, 2021
+///
+/// Problem Statement: Creates a counter class that can be used in Main. Main program will update the counter inside the counter class.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Declare public class Counter
+/// 2) Declare the methods inside, along with the private scope variable count that will be used inside.
+/// 3) Use each method in main to change the counter variable in Counter
+
+using System;
+
+namespace Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter count = new Counter();
+
+            count.setCounter();
+            Console.WriteLine(count.displayCount());
+
+            count.incCounter();
+            Console.WriteLine(count.displayCount());
+
+            count.incCounter();
+            Console.WriteLine(count.displayCount());
+
+            Counter countTwo = new Counter();
+            countTwo.setCounter();
+            countTwo.incCounter();
+            Console.WriteLine("Counter is " + count.getCount());
+            Console.WriteLine("Equals method used: " + count.Equals(countTwo));
+
+            count.dscCounter();
+
+            Console.WriteLine("Counter is " + count.toString());
+            Console.WriteLine("Second Counter is " + countTwo.toString());
+
+            Console.WriteLine("Equals method used again: " + count.Equals(countTwo));
+            
+        }
+    }
+}
